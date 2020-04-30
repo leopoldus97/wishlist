97bac04db01713c1bce7f3743a338d30783d4fe7
Marek Stancik <43235061+MarekStancik@users.noreply.github.com>
2020-04-30 16:13:01 +0200

    Fixing login background color
    Note - body.login style has to be in styles.css otherwise it does not work

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit,AfterViewInit,OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngAfterViewInit() {
+    document.querySelector('body').classList.add('login');
+  }
+
+  ngOnDestroy() {
+    document.querySelector('body').classList.remove('login');
+}
 
 }
